485a91070d2493d3a83a12dec7e64c6e56e17766
waschbaer0395 <rene.hippe@study.hs-duesseldorf.de>
2021-12-12 05:19:02 +0100

    added buttons

--- src/ext/confirmer.py
+++ src/ext/confirmer.py
@@ -1,99 +1,56 @@
 import discord
-import asyncio
-
-from collections import OrderedDict
+from discord import abc
+from discord.interactions import Interaction
+from discord.utils import MISSING
+from discord.commands import ApplicationContext
+from discord.ext.commands import Context
 
 
 class ConfirmerSession:
     """Class that interactively paginates
     a set of embed using reactions."""
 
-    def __init__(self, ctx, timeout=60, page='', color=discord.Color.green(), footer=''):
-        """Confirmer init."""
-        self.footer = footer  # footer message
-        self.ctx = ctx  # ctx
-        self.timeout = timeout  # when the reactions get cleared, int[seconds]
+    def __init__(self, page, color=discord.Color.green(), footer=''):
+        """Confirmer, for confirming things obv duh."""
+        super().__init__()
         self.page = page  # the list of embeds list[discord.Embed, discord.Embed]
-        self.running = False  # currently running, bool
-        self.message = None  # current message being paginated, discord.Message
-        self.current = 0  # current page index, int
-        self.color = color  # embed color
-        # can't be awaited here, must be done in PaginatorSession.run()
-        self.reactions = OrderedDict({
-            '✅': self.accept,
-            '❌': self.deny
-        })
 
-    async def show_page(self):
-
-        page = self.page  # gets the page
-        page.set_footer(text=self.footer)  # sets footer
-
-        if self.running:
-            # if the first embed was sent, it edits it
-            await self.message.edit(embed=page)
+    async def run(self, ctx):
+        """Asks the user a question to confirm something."""
+        # We create the view and assign it to a variable so we can wait for it later.
+        view = Confirm()
+        message = await ctx.send(embed=self.page, view=view)
+        # Wait for the View to stop listening for input...
+        await view.wait()
+        message = await message.edit(embed=self.page, view=view)
+        if view.value is None:
+            return False, message
+        elif view.value:
+            return True, message
         else:
-            self.running = True
-            # sends the message
-            self.message = await self.ctx.send(embed=page)
+            return False, message
 
-            # adds reactions
-            for reaction in self.reactions.keys():
-                await self.message.add_reaction(reaction)
 
-    def react_check(self, reaction, user):
-        """Check to make sure it only responds to reactions from the sender and on the same message."""
-        if reaction.message.id != self.message.id:
-            return False  # not the same message
-        if user.id != self.ctx.author.id:
-            return False  # not the same user
-        if reaction.emoji in self.reactions.keys():
-            return True  # reaction was one of the confirmer emojis
+# Define a simple View that gives us a confirmation menu
+class Confirm(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
 
-    async def run(self):
-        """Actually runs the confirmer session."""
-        if not self.running:
-            # display the embed
-            await self.show_page()
-        while self.running:
-            try:
-                # waits for reaction using react_check
-                reaction, user = await self.ctx.bot.wait_for('reaction_add', check=self.react_check, timeout=self.timeout)
-            except asyncio.TimeoutError:
-                self.running = False
-                try:
-                    await self.message.clear_reactions()  # tries to remove reactions
-                except Exception as e:
-                    print(e)
-                    break
-            else:
-                # same as above
-                try:
-                    await self.message.remove_reaction(reaction, user)
-                except Exception as e:
-                    print(e)
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = False
+        self.stop()
+        self.remove_item(self.children[0])
+        self.remove_item(self.children[0])
 
-                action = self.reactions[reaction.emoji]  # gets the function from the reaction map OrderedDict
-                response = await action()  # awaits here with () because __init__ can't be async
-                return response, self.message
-
-    # all functions with await must be async
-    async def accept(self):
-        """Accept the prompt."""
-        try:
-            await self.message.clear_reactions()
-        except Exception as e:
-            print(e)
-
-        self.running = False
-        return True
-
-    async def deny(self):
-        """Deny the prompt."""
-        try:
-            await self.message.clear_reactions()
-        except Exception as e:
-            print(e)
-
-        self.running = False
-        return False
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = True
+        self.stop()
+        self.remove_item(self.children[0])
+        self.remove_item(self.children[0])
